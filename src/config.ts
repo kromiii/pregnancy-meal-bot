42006e275b5bb3185ef529596d057c8e5b633733
Hiroyuki Kuromiya <15026387+kromiii@users.noreply.github.com>
2024-08-31 14:19:34 +0900

    remove config from perplexity api

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,4 @@
 export const config = {
-  PERPLEXITY_APIKEY:
-    PropertiesService.getScriptProperties().getProperty("PERPLEXITY_APIKEY"),
   LINE_ACCESS_TOKEN:
     PropertiesService.getScriptProperties().getProperty("LINE_ACCESS_TOKEN"),
   OPENAI_API_KEY:
